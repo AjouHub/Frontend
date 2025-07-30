28e7e354e055bfc852376ccc8ef88a600604bc33
eatingshark <akqmfzld@ajou.ac.kr>
2025-07-31 01:21:01 +0900

    fix : 학과 정보 한글 출력

--- src/pages/AccountInfoPage/index.tsx
+++ src/pages/AccountInfoPage/index.tsx
@@ -53,13 +53,32 @@ function AccountInfo(): JSX.Element {
             </div>
         );
     }
+    const departmentNameMap: { [key: string]: string } = {
+        'department.me': '기계공학과',
+        'department.env': '환경안전공학과',
+        'department.academic': '산업공학과',
+        'department.ce': '건설시스템공학과',
+        'department.che': '화학공학과',
+        'department.tse': '교통시스템공학과',
+        'department.mse': '첨단신소재공학과',
+        'department.arch': '건축학과',
+        'department.chembio': '응용화학생명공학과',
+        'department.ise': '융합시스템공학과',
+        'department.appchem': '응용화학과',
+    };
 
     return (
         <div style={{ padding: '20px' }}>
             <h2>계정 정보</h2>
             <p><strong>이름:</strong> {user.name}</p>
             <p><strong>이메일:</strong> {user.email}</p>
-            <p><strong>학과:</strong> {user.departments.join(', ')}</p> <br/>
+            <p>
+                <strong>학과: </strong>
+                {user.departments
+                    .filter(dep => dep && dep !== 'DefaultDept')
+                    .map(dep => departmentNameMap[dep] || dep)
+                    .join(', ')}
+            </p>
             <NavigationButton to='/mypage'>홈으로</NavigationButton>
         </div>
     );
